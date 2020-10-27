340cd4e71236549bb7d256bd33e2e4e2e515c0f0
Kaustubh Ghodke <kaustubhdgr8@gmail.com>
2020-10-27 23:22:20 +0530

    added menu and footer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,74 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
-    concat: {
-      js: {
-        src: ['src/scripts/*.js', '!src/scripts/app.js'],
-        dest: 'src/scripts/app.js'
-      }
-    },
     babel: {
       options: {
-        sourceMap: true
+        sourceMap: true,
       },
       dist: {
         files: {
-          "public/app.js": "src/scripts/app.js"
-        }
-      }
+          "public/app.js": "src/scripts/app.js",
+        },
+      },
+    },
+    bake: {
+      html: {
+        files: {
+          "src/templates/index.html": "src/templates/base.html",
+        },
+      },
+    },
+    concat: {
+      js: {
+        src: ["src/scripts/*.js", "!src/scripts/app.js"],
+        dest: "src/scripts/app.js",
+      },
     },
     copy: {
-      files: {
+      html: {
         expand: true,
         flatten: true,
-        cwd: "src",
-        src: ["**/*", "!**/*.js"],
-        dest: "public/"
-      }
+        src: ["src/templates/index.html", "src/templates/404.html"],
+        dest: "public/",
+      },
+      css: {
+        expand: true,
+        flatten: true,
+        src: ["src/styles/styles.css"],
+        dest: "public/",
+      },
+      assets: {
+        expand: true,
+        flatten: false,
+        src: ["assets/**/*"],
+        dest: "public/",
+      },
     },
     jshint: {
-      all: ['Gruntfile.js', 'src/**/*.js'],
+      all: ["Gruntfile.js", "src/**/*.js"],
       options: {
-        'esversion': 6
-      }
+        esversion: 6,
+      },
     },
     watch: {
       gruntfile: {
         files: "Gruntfile.js",
-        tasks: ["jshint"]
+        tasks: ["jshint"],
       },
       src: {
-        files: ["src/**/*"],
-        tasks: ["compile"]
-      }
-    }
+        files: ["src/**/*", "!src/templates/index.html", "!src/scripts/app.js"],
+        tasks: ["compile"],
+      },
+    },
   });
 
   grunt.loadNpmTasks("grunt-babel");
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks("grunt-bake");
+  grunt.loadNpmTasks("grunt-contrib-concat");
   grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
   grunt.registerTask("default", ["compile", "watch"]);
-  grunt.registerTask("compile", ["jshint", "concat", "babel", "copy"]);
+  grunt.registerTask("compile", ["jshint", "concat", "babel", "bake", "copy"]);
 };
